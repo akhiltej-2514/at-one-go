70c4660c4c615f487c1fa1c922bd8ced57961f39
akhiltej-2514 <akhiltej.k20@iiits.in>
2022-05-13 08:57:58 +0530

    routing matrix

---
+++ src/App.js
@@ -0,0 +1,177 @@
+import { useRef, useEffect, useState } from 'react'
+import * as tt from '@tomtom-international/web-sdk-maps'
+import * as ttapi from '@tomtom-international/web-sdk-services'
+import './App.css'
+import '@tomtom-international/web-sdk-maps/dist/maps.css'
+
+const App = () => {
+  const mapElement = useRef()
+  const [map, setMap] = useState({})
+  const [longitude, setLongitude] = useState(77.594566)
+  const [latitude, setLatitude] = useState(12.971599)
+
+  const convertToPoints = (lngLat) => {
+    return {
+      point: {
+        latitude: lngLat.lat,
+        longitude: lngLat.lng
+      }
+    }
+  }
+
+  const drawRoute = (geoJson, map) => {
+    if (map.getLayer('route')) {
+      map.removeLayer('route')
+      map.removeSource('route')
+    }
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: geoJson
+      },
+      paint: {
+        'line-color': 'blue',
+        'line-width': 6
+  
+      }
+    })
+  }
+
+  const addDeliveryMarker = (lngLat, map) => {
+    const element = document.createElement('div')
+     element.className = 'marker-delivery'
+    new tt.Marker({
+      element: element
+    })
+    .setLngLat(lngLat)
+    .addTo(map)
+  }
+
+  useEffect(() => {
+    const origin = {
+      lng: longitude,
+      lat: latitude,
+    }
+    const destinations = []
+
+    let map = tt.map({
+      key: "RKqZoufGP8qNritQfUA4AZDr3lM7GTCR",
+      container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
+      center: [longitude, latitude],
+      zoom: 14,
+    })
+    setMap(map)
+
+    const addMarker = () => {
+      const popupOffset = {
+        bottom: [0, -25]
+      }
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML('This is you!')
+      const element = document.createElement('div')
+      element.className = 'marker'
+
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+        .setLngLat([longitude, latitude])
+        .addTo(map)
+      
+      marker.on('dragend', () => {
+        const lngLat = marker.getLngLat()
+        setLongitude(lngLat.lng)
+        setLatitude(lngLat.lat)
+      })
+
+      marker.setPopup(popup).togglePopup()
+      
+    }
+    addMarker()
+
+    const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination)
+      })
+      const callParameters = {
+        key:"RKqZoufGP8qNritQfUA4AZDr3lM7GTCR",
+        destinations: pointsForDestinations,
+        origins: [convertToPoints(origin)],
+      }
+
+    return new Promise((resolve, reject) => {
+      ttapi.services
+        .matrixRouting(callParameters)
+        .then((matrixAPIResults) => {
+          const results = matrixAPIResults.matrix[0]
+          const resultsArray = results.map((result, index) => {
+            return {
+              location: locations[index],
+              drivingtime: result.response.routeSummary.travelTimeInSeconds,
+            }
+          })
+          resultsArray.sort((a, b) => {
+            return a.drivingtime - b.drivingtime
+          })
+          const sortedLocations = resultsArray.map((result) => {
+            return result.location
+          })
+          resolve(sortedLocations)
+        })
+      })
+    }
+
+    const recalculateRoutes = () => {
+      sortDestinations(destinations).then((sorted) => {
+        sorted.unshift(origin)
+
+        ttapi.services
+          .calculateRoute({
+            key:"RKqZoufGP8qNritQfUA4AZDr3lM7GTCR",
+            locations: sorted,
+          })
+          .then((routeData) => {
+            const geoJson = routeData.toGeoJson()
+            drawRoute(geoJson, map)
+        })
+      })
+    }
+
+
+    map.on('click', (e) => {
+      destinations.push(e.lngLat)
+      addDeliveryMarker(e.lngLat, map)
+      recalculateRoutes()
+    })
+
+    return () => map.remove()
+  }, [longitude, latitude])
+
+  return (
+    <>
+      {map&&<div className="app">
+            <div ref={mapElement} className="map" />
+            <div className="searcho">
+              <span>Have coordinates ? Find here :-</span>
+              <input id='lat' type="text" placeholder="latitude" />
+              <input id ='long' type="text" placeholder="longitude" />
+              <button onClick={()=>{
+                setLatitude(document.getElementById('lat').value);
+                setLongitude(document.getElementById('long').value);
+              }}>Search</button>
+              <div className="show">
+              Current latitude<span id="showlat">{latitude}</span>
+              Current longitude<span id="showlong">{longitude}</span>
+              </div>
+            </div>
+    </div>}
+    </>
+  )
+}
+
+export default App
